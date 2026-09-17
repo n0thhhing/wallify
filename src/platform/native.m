4b806f61bebeb34e6b2b552852ca0cdac6211299
Levi <lynchlm064@gmail.com>
2026-09-17 11:40:54 -0400

    ui: switch native glass to UnderWindowBackground with VibrantDark appearance to perfectly match macOS Sonoma desktop widgets

--- src/platform/native.m
+++ src/platform/native.m
@@ -451,7 +451,8 @@ void wallify_update_glass_rect(double x, double y, double w, double h, double ra
         if (active) {
             if (!globalGlassView) {
                 globalGlassView = [[NSVisualEffectView alloc] initWithFrame:NSZeroRect];
-                globalGlassView.material = NSVisualEffectMaterialPopover;
+                globalGlassView.material = NSVisualEffectMaterialUnderWindowBackground;
+                globalGlassView.appearance = [NSAppearance appearanceNamed:NSAppearanceNameVibrantDark];
                 globalGlassView.blendingMode = NSVisualEffectBlendingModeBehindWindow;
                 globalGlassView.state = NSVisualEffectStateActive;
                 globalGlassView.wantsLayer = YES;
