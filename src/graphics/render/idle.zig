0f5eaab2cfc8e453e79d1933c9cb11961eade590
n0thhhing <137664746+n0thhhing@users.noreply.github.com>
2026-09-20 18:20:24 -0400

    fix(render): remove obsolete layout alias

--- src/graphics/render/idle.zig
+++ src/graphics/render/idle.zig
@@ -3,7 +3,6 @@ const state = @import("../../state.zig");
 const gpu = @import("../canvas.zig");
 const text = @import("../text_cache.zig");
 
-const L = state.Layout;
 const primary_color: gpu.Color = .{ 245.0 / 255.0, 245.0 / 255.0, 247.0 / 255.0, 1.0 };
 const subtitle_color: gpu.Color = .{ 0.62, 0.60, 0.57, 1.0 };
 const idle_card_bg: gpu.Color = .{ 30.0 / 255.0, 29.0 / 255.0, 32.0 / 255.0, 1.0 };
