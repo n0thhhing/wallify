99159554d0735d781f62bd37f8f3bb08944a9677
n0thhhing <137664746+n0thhhing@users.noreply.github.com>
2026-09-20 21:36:29 -0400

    fix: link libc++ for debug ImGui

--- build.zig
+++ build.zig
@@ -49,6 +49,7 @@ pub fn build(b: *std.Build) void {
     if (debug_inspector) {
         const imgui_dir = ".zig-cache/wallify-imgui";
         mod.addIncludePath(b.path(imgui_dir));
+        mod.linkSystemLibrary("c++", .{});
         mod.addIncludePath(b.path(b.fmt("{s}/backends", .{imgui_dir})));
         mod.linkFramework("MetalKit", .{});
         mod.linkFramework("GameController", .{});
