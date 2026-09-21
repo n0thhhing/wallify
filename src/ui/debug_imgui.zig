0d2d6108c357b15ff65c68034e39548c5d62aefd
n0thhhing <137664746+n0thhhing@users.noreply.github.com>
2026-09-20 21:16:08 -0400

    feat(debug): add optional Dear ImGui inspector support

---
+++ src/ui/debug_imgui.zig
@@ -0,0 +1,10 @@
+pub extern fn wallify_imgui_inspector_show() void;
+pub extern fn wallify_imgui_inspector_hide() void;
+
+pub fn show() void {
+    wallify_imgui_inspector_show();
+}
+
+pub fn hide() void {
+    wallify_imgui_inspector_hide();
+}
